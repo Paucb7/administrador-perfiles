3d60bd4e74ab82f27156dbd3b478840865a4b4d8
Paulina <112720866+Paucb7@users.noreply.github.com>
2023-05-03 00:24:52 -0600

    second commit

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
     $conn = mysqli_init();
     $conn->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
     $conn->real_connect($_ENV["HOST"], $_ENV["USERNAME"], $_ENV["PASSWORD"], $_ENV["DATABASE"]);
-    $conn->close();
+    //$conn->close();
 ?>
