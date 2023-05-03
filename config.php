1edcc402a9678548d9f392a056d5dd9af9a50c0f
PCoronaBarba <paulinacbarba7@gmail.com>
2023-05-03 00:04:24 -0600

    first commit

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
-<?php 
-
-$conn = mysqli_connect('localhost', 'root', '', 'user_db') or die('connection failed');
-
+<?php
+    $conn = mysqli_init();
+    $conn->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
+    $conn->real_connect($_ENV["HOST"], $_ENV["USERNAME"], $_ENV["PASSWORD"], $_ENV["DATABASE"]);
+    $conn->close();
 ?>
